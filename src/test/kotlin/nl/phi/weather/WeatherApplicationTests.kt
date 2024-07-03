02302bf7a1e67b8c624daca6b237f9a2d61c2d73
Phi Tiet <anhphi099@gmail.com>
2024-07-03 15:17:52 +0200

    verfies introducing test app yml works

--- src/test/kotlin/nl/phi/weather/WeatherApplicationTests.kt
+++ src/test/kotlin/nl/phi/weather/WeatherApplicationTests.kt
@@ -1,14 +1,10 @@
 package nl.phi.weather
 
-import nl.phi.weather.service.EmailService
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
 
 @SpringBootTest
 class WeatherApplicationTests {
-    @MockBean
-    private lateinit var emailService: EmailService
 
     @Test
     fun contextLoads() {
